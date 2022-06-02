92ad2088e7560ac7fceddaf64a523ff9b44b5869
DaSim6 <learner-9f5db4@extensionschool.ch>
2022-06-02 16:09:40 +0200

    LoopYourSongs Py

---
+++ LoopYourSongs.py
@@ -0,0 +1,9 @@
+fav_songs = ["Guns N'Roses - Sweet child 'O Mine", "AC/DC - Thunderstruck"]
+fav_songs.append("La India - Vivir lo nuestro")
+fav_songs.extend(["Eagles - Hotel California", "Scorpions - Wind of changes"])
+
+print("These are my favorite songs:")
+for i in range(len(fav_songs)):
+    print(fav_songs[i])
+print("What are yours ?")
+
